ed67eefac57141ca43b666aad2c766f6cc07f1bd
Kartono666 <Kartono.saleh@gmail.com>
2024-05-30 23:18:38 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
     protected $table = "users";
     protected $primaryKey = "id";
     protected $keyType = "int";
+    protected $fillable = ['username', 'password','name'];
     public $timestamps = true;
     public $incrementing = true;
+
 
 
     public function contacts() : HasMany {
